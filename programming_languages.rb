ab75521361fd4414481b63182376d346b84dac3c
Ignacio R Espino <irespino@capozios.com>
2018-09-10 01:35:30 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,8 +1,13 @@
 def reformat_languages(languages)
   new_hash = {}
+  style_array = {}
   
   languages.values.each do |x|
     new_hash = new_hash.merge!(x)
+  end
+  
+  languages.values.keys.each do |x|
+    style_array << x
   end
   
   new_hash
