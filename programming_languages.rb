f1a51b552cd7d56b58660664e55e7b72d0e059bd
Ignacio R Espino <irespino@capozios.com>
2018-09-10 02:03:22 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,22 @@
+require 'pry'
+
 def reformat_languages(languages)
-  # your code here
+  new_hash = {}
+  style_array_oo = languages[:oo].keys
+  style_array_functional = languages[:functional].keys
+  
+  languages.values.each do |x|
+    new_hash = new_hash.merge!(x)
+  end
+  
+  new_hash.keys.each do |x|
+    if style_array_oo.include?(x) && style_array_functional.include?(x)
+      new_hash[x][:style] = [:oo, :functional]
+    elsif style_array_functional.include?(x)
+      new_hash[x][:style] = [:functional]
+    elsif style_array_oo.include?(x)
+      new_hash[x][:style] = [:oo]
+    end
+  end
+  new_hash
 end
