9da7fb89875ebad46d50c5fbb86cad0b933df897
Ignacio R Espino <irespino@capozios.com>
2018-09-10 01:56:03 +0000

    automatically backed up by learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -8,6 +8,14 @@ def reformat_languages(languages)
   end
   
   new_hash.keys.each do |x|
+    if style_array_oo.includes?(x) && style_array_functional.inclues?(x)
+      new_hash[x][:style] = [:oo, :functional]
+    elsif style_array_functional.includes?(x)
+      new_hash[x][:styles] = [:functional]
+    elsif style_array_oo.includes?(x)
+      new_hash[x][:styles] = [:oo]
+    end
+    
     new_hash[x][:style] = nil
   end
 
